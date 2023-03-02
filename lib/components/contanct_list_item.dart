1f704a76755c5677808f379c16d421fe8bd3d1a3
Sebastiano <sebastiano.gaudeano@live.com>
2023-03-02 15:48:12 +0100

    divided in component

---
+++ lib/components/contanct_list_item.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class ContactListItem extends StatelessWidget {
+  final String srcProfile;
+  final String firstName;
+  final String lastName;
+  final String cell;
+
+  const ContactListItem(
+      {super.key,
+      required this.srcProfile,
+      required this.firstName,
+      required this.lastName,
+      required this.cell});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+              child: CircleAvatar(
+                radius: 40,
+                foregroundImage: NetworkImage(srcProfile),
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(text: firstName),
+                      const TextSpan(text: " "),
+                      TextSpan(text: lastName)
+                    ],
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.w700),
+                  ),
+                ),
+                Text(
+                  textAlign: TextAlign.left,
+                  cell,
+                  style: const TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w300,
+                      color: Colors.grey),
+                ),
+              ],
+            ),
+          ],
+        ),
+        const Padding(
+          padding: EdgeInsets.only(right: 20),
+          child: Icon(Icons.phone_enabled_rounded),
+        )
+      ],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:address_book/models/models.dart';
+import 'package:address_book/components/contanct_list_item.dart';
 import 'package:address_book/services/people_service.dart';
 import 'package:flutter/material.dart';
 
@@ -13,60 +13,17 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final data = PeopleService().getPeople(results: 10).toList();
 
-    Widget mapItemToListView(Person p) {
-      //p.picture.medium
-      return Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
-            children: [
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
-                child: CircleAvatar(
-                  radius: 40,
-                  foregroundImage: NetworkImage(p.picture!.medium!),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text.rich(
-                    TextSpan(
-                      children: [
-                        TextSpan(text: p.firstName!),
-                        const TextSpan(text: " "),
-                        TextSpan(text: p.lastName!)
-                      ],
-                      style: const TextStyle(
-                          fontSize: 16, fontWeight: FontWeight.w700),
-                    ),
-                  ),
-                  Text(
-                    textAlign: TextAlign.left,
-                    p.cell!,
-                    style: const TextStyle(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w300,
-                        color: Colors.grey),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          const Padding(
-            padding: EdgeInsets.only(right: 20),
-            child: Icon(Icons.phone_enabled_rounded),
-          )
-        ],
-      );
-    }
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: ListView(
-          children: data.map(mapItemToListView).toList(),
+          children: data
+              .map((e) => ContactListItem(
+                  srcProfile: e.picture!.medium!,
+                  firstName: e.firstName!,
+                  lastName: e.lastName!,
+                  cell: e.cell!))
+              .toList(),
         ),
       ),
     );
